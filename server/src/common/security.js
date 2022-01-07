5ca00c2f3c3e28791d208f5bb39f84a9f54776f5
michal16295 <michal16292@gmail.com>
2022-01-07 16:25:36 +0200

    server security

---
+++ server/src/common/security.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const config = require("../config/eyes.json");
+const UrlPattern = require("url-pattern");
+
+// This will sign with data and key from config and return the JWT
+module.exports.signJwt = (data) => {
+  return jwt.sign(data, config.jwtPrivateKey);
+};
+
+function isUrlAllowed(req, urls) {
+  const url = req.url.split("?")[0];
+  for (let i = 0; i < urls.length; ++i) {
+    let obj = urls[i];
+    let pattern = new UrlPattern(obj.url);
+    if (pattern.match(url)) {
+      if (req.method === obj.method) return true;
+    }
+  }
+  return false;
+}
+
+// Hash the password using bcrypt
+module.exports.crypt = async (password) => {
+  const saltRounds = 10;
+  const hashed = await bcrypt.hash(password, saltRounds);
+  return hashed;
+};
+
+// Validate password
+module.exports.validatePassword = async (password1, password2) => {
+  return await bcrypt.compare(password1, password2);
+};
+
+module.exports.verifyToken = (token) => {
+  return jwt.verify(token, config.jwtPrivateKey);
+};
